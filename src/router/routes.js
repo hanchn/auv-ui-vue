80a3c167b54318f44eeeb1d0aba576a5c01c1253
yuanjing <jing.yuan@kapeixi.com>
2025-01-22 15:14:03 +0800

    .

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,17 +1,11 @@
 // Auto-generated routes
 import { defineAsyncComponent } from 'vue';
 
-const AAA = defineAsyncComponent(() => import('@/views/AAA/index.vue'));
 const Home = defineAsyncComponent(() => import('@/views/Home/index.vue'));
 
 const routes = [
   {
-    path: '/AAA',
-    name: 'AAA',
-    component: AAA
-  },
-  {
-    path: '/Home',
+    path: '/Home/index.vue',
     name: 'Home',
     component: Home
   }
---
+++ src/views/AuvInput/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="auv-input">
+    <input type="text" placerholder="请输入"/>
+  </div>
+</template>
+<script>
+export default {
+  name: 'AuvInput'
+}
+</script>
