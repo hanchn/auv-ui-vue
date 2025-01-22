f38d189a2bd00e0042ec64530b2ecdff924bb2b0
yuanjing <jing.yuan@kapeixi.com>
2025-01-22 10:49:39 +0800

    .

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import { fileURLToPath } from 'url';
 import path from 'path';
 
+// 获取当前文件的路径
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
